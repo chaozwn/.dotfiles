2e1d242aa014a45554bf2afcaa26c3b0498c784e
jayce.zhao <326747337@qq.com>
2025-01-17 15:31:52 +0800

    feat: add fonts

--- scripts/install_kitty.sh
+++ scripts/install_kitty.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono-nerd-font
+# 获取系统相关信息
+# Get system information
+get_system_info() {
+  OS_TYPE="$(uname)"
+}
+
+# 根据系统类型确定 brew 路径
+# Determine brew path based on OS
+install_fonts() {
+  if [ "$OS_TYPE" = "Darwin" ]; then
+    brew tap homebrew/cask-fonts
+    brew install --cask font-jetbrains-mono-nerd-font
+  else
+    mkdir -p ~/.local/share/fonts
+    cp ~/.dotfiles/fonts/*.ttf ~/.local/share/fonts/
+  fi
+}
