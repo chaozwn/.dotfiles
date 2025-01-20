af2692d3e0bf53e734496bfa8106a67cb5925c86
jayce.zhao <326747337@qq.com>
2025-01-20 09:54:39 +0800

    feat: replace

--- nvim/lua/plugins/rainbow-delimiters.lua
+++
@@ -1,7 +0,0 @@
----@type LazySpec
-return {
-  "HiPhish/rainbow-delimiters.nvim",
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  event = "User AstroFile",
-  main = "rainbow-delimiters.setup",
-}
