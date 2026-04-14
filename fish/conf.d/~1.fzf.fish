f1fe6c8c38429241dfc63c7bd6d84399b4532d34
zhao_wei_nan <326747337@qq.com>
2026-04-14 10:08:57 +0800

    feat: updtae

--- fish/conf.d/~1.fzf.fish
+++ fish/conf.d/~1.fzf.fish
@@ -2,17 +2,7 @@
 source ~/.config/fish/themes/tokyonight_moon.sh
 
 # 设置fzf界面为60%
-set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS 
-  --cycle
-  --layout=reverse
-  --height 60%
-  --ansi
-  --preview-window=right:70%
-  --bind=ctrl-u:half-page-up,ctrl-d:half-page-down,ctrl-x:jump
-  --bind=ctrl-f:preview-page-down,ctrl-b:preview-page-up
-  --bind=ctrl-a:beginning-of-line,ctrl-e:end-of-line
-  --bind=ctrl-j:down,ctrl-k:up
-"
+set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --cycle --layout=reverse --height 60% --ansi --preview-window=right:70% --bind=ctrl-u:half-page-up,ctrl-d:half-page-down,ctrl-x:jump --bind=ctrl-f:preview-page-down,ctrl-b:preview-page-up --bind=ctrl-a:beginning-of-line,ctrl-e:end-of-line --bind=ctrl-j:down,ctrl-k:up"
 
 # 设置diffview的highlighter
 set fzf_diff_highlighter delta --paging=never --width=20
