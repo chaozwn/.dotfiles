9e6156004048e582a1dd194e3985c658b80f8dde
zhao_wei_nan <326747337@qq.com>
2026-05-07 23:50:48 +0800

    feat: update

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,10 @@
 function fish_greeting
     if not status is-interactive
-        exit
+        return
     end
-    fastfetch
+
+    # fastfetch can occasionally block terminal startup while probing system state.
+    if test "$DOTFILES_FASTFETCH_ON_STARTUP" = 1
+        fastfetch
+    end
 end
