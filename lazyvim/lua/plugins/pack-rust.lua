a6835a926cd0f073dda4794852cd897543a259f1
zhaown <326747337@qq.com>
2026-05-10 23:16:26 +0800

    feat: add rust

---
+++ lazyvim/lua/plugins/pack-rust.lua
@@ -0,0 +1,13 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "rust",
+      root = {
+        "Cargo.toml",
+        "rust-project.json"
+      },
+    })
+  end,
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  {},
+}
